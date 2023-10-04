dfbfab3a7aafea3c5a9f0c6ab169788c8df5a64f
Zainabsoomro6789 <Zainabsoomro6789@gmail.com>
2023-10-05 03:41:04 +0500

    ready to commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// array 
+// q1
+// var name = ["zaianb" , "hiba"];
+// alert(name);
+// q2
+// var myName = ["zainab hassan "];
+// alert(myName);
+// // Q3
+// var numb = [32];
+// alert(numb);
+// q4
+// var xyx = ["hiba",false, 45]
+// alert(xyx);
+// q5
+// var degree = ["BS" ,"BCOM","MPhill"];
+// document.write(degree);
+// Q6
+// var color = ["red", "yellow","black", "white","purple", "blue"];
+//  document.write(color);
+// / Q 11
+// var cityName = ["karachi","lahore","Islamabad","Quetta","Peshawar"];
+// console.log("original array", cityName);
+
+// var StartingValue = prompt("enter StartingValue");
+// var EndingValue = prompt("enter endingValue");
+// var copy = cityName.splice(StartingValue,EndingValue);
+// console.log("updated", copy);
+// Q 15
+// var arr = ["Samsung","Motorola","Nokia","vivo","Sony"];
+// document.write("<select>" + "<option>" + arr[0] + "</option>" +
+// "<option>" + arr[1] + "</option>"  +
+// "<option>" + arr[2] + "</option>"  +
+// "<option>" + arr[3] + "</option>"  +
+// "<option>" + arr[4] + "</option>"  +
+// "<option>" + arr[5] + "</option>"  +
+// "</select>")
